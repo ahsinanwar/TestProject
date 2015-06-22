8ea2dffdc46ec16b637d103a788771a965c358dd
asad <asadullahbeg@gmail.com>
2015-06-22 12:46:00 +0500

    MAJOR FUCK UP

--- TimeAttendanceSystem/ViewModels/VMDashboard/VMDashboard.cs
+++ TimeAttendanceSystem/ViewModels/VMDashboard/VMDashboard.cs
@@ -5,17 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeAttendanceSystem.BaseClasses;
+using TimeAttendanceSystem.Model;
 
 namespace TimeAttendanceSystem.ViewModels.VMDashboard
 {
-    class VMDashboard
+    class VMDashboard : ObservableObject
     {
-        private ObservableCollection<_2dValues> _presence;
-       
+        private ObservableCollection<DailySummary> _presence;
+        TAS2013Entities context;
+        public ObservableCollection<DailySummary> presence { get{
+            return _presence;
+        
+        } set{
+            _presence = value;
+            OnPropertyChanged("presence");
+        } }
         #region constructor
         public VMDashboard()
         {
-          
+            context = new TAS2013Entities();
+            _presence = new ObservableCollection<DailySummary>(context.DailySummaries);
+            OnPropertyChanged("_presence");
         }
         #endregion
     }
