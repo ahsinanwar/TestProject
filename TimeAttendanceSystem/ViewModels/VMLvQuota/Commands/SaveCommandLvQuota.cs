ee2c5cc4416d879665cf2ef9e3ca1bc8f702883c
engrawais <awais.cns@gmail.com>
2015-11-12 16:56:20 +0500

    leave consumed

--- TimeAttendanceSystem/ViewModels/VMLvQuota/Commands/SaveCommandLvQuota.cs
+++ TimeAttendanceSystem/ViewModels/VMLvQuota/Commands/SaveCommandLvQuota.cs
@@ -51,7 +51,11 @@ namespace TimeAttendanceSystem.ViewModels.VMLvQuota.Commands
                     else
                     {
                         LvQuota transition = new LvQuota();
+                        
+                       
+
                         transition = vmd.selectedLvQuota;
+                        PopulateLvConsumed(transition);
                         transition.Emp = null;
                         context.LvQuotas.Add(transition);
                         context.SaveChanges();
@@ -65,7 +69,7 @@ namespace TimeAttendanceSystem.ViewModels.VMLvQuota.Commands
             else
             {
                 LvQuota lvquota = context.LvQuotas.First(aa => aa.EmpID == vmd.selectedLvQuota.EmpID);
-               
+             
                 vmd.isEnabled = false;
                 vmd.isAdding = false;
                 context.SaveChanges();
@@ -73,6 +77,64 @@ namespace TimeAttendanceSystem.ViewModels.VMLvQuota.Commands
             }
            
             
+        }
+
+        private void PopulateLvConsumed(LvQuota transition)
+        {
+           
+            for (int i = 0; i <= 2 ; i++)
+            {
+                int caseSwitch = i;
+                LvConsumed annualleave = new LvConsumed();
+                annualleave.JanConsumed = 0;
+                annualleave.FebConsumed = 0;
+                annualleave.MarchConsumed = 0;
+                annualleave.AprConsumed = 0;
+                annualleave.MayConsumed = 0;
+                annualleave.JuneConsumed = 0;
+                annualleave.JulyConsumed = 0;
+                annualleave.AugustConsumed = 0;
+                annualleave.SepConsumed = 0;
+                annualleave.OctConsumed = 0;
+                annualleave.NovConsumed = 0;
+                annualleave.DecConsumed = 0;
+                switch (caseSwitch)
+                {
+                    case 0:
+                        annualleave.LeaveType = "A";
+                        annualleave.EmpLvType = transition.EmpID + "A";
+                        annualleave.EmpID = transition.EmpID;
+                        annualleave.GrandTotal = transition.A;
+                        annualleave.GrandTotalRemaining = transition.A;
+                        annualleave.YearRemaining = transition.A;
+                        annualleave.TotalForYear = transition.A;
+                        
+                        break;
+                    case 1:
+                        annualleave.LeaveType = "B";
+                        annualleave.EmpLvType = transition.EmpID + "B";
+                        annualleave.EmpID = transition.EmpID;
+                        annualleave.GrandTotal = transition.B;
+                        annualleave.GrandTotalRemaining = transition.B;
+                        annualleave.YearRemaining = transition.B;
+                        annualleave.TotalForYear = transition.B;
+                        
+                        break;
+                    case 2:
+                        annualleave.LeaveType = "C";
+                        annualleave.EmpLvType = transition.EmpID + "C";
+                        annualleave.EmpID = transition.EmpID;
+                        annualleave.GrandTotal = transition.C;
+                        annualleave.GrandTotalRemaining = transition.C;
+                        annualleave.YearRemaining = transition.C;
+                        annualleave.TotalForYear = transition.C;
+                        
+                        break;
+                }
+                context.LvConsumeds.Add(annualleave);
+                context.SaveChanges();
+            }
+
         }
         #endregion
     }
