7e6d3f336d96df4846117af91580170e6488fef0
engrawais <awais.cns@gmail.com>
2015-11-13 14:43:38 +0500

    lv app process

--- TimeAttendanceSystem/Model/Model2.Designer.cs
+++ TimeAttendanceSystem/Model/Model2.Designer.cs
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 ﻿// Default code generation is disabled for model 'D:\invenprojects\TimeAttendanceSystem\TimeAttendanceSystem\Model\Model2.edmx'. 
-=======
-﻿// Default code generation is disabled for model 'C:\Track\TimeAttendanceSystem\TimeAttendanceSystem\Model\Model2.edmx'. 
->>>>>>> 5c616885730175871b868d1534c0e91501656ed3
+
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
